c24d78c08917a36f0175f58d3e8819abc5bdd24e
Aryan Babare <babreab17@gmail.com>
2025-04-05 01:36:11 +0530

    fixed ui

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,19 +21,20 @@ const About = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-globe size-3"><circle cx="12" cy="12" r="10"></circle><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path><path d="M2 12h20"></path></svg>
                                 Pune,India
                             </div>
+                            
                             <div className='flex gap-1 mt-1'>
-                                <a href="" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
+                                <a href="mailto:aryanbabare1@gmail.com" target="_blank" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6" >
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                                     </svg>
                                 </a>
-                                <a href="" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
+                                <a href="https://github.com/aryanx16" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
                                 </a>
-                                <a href="" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
+                                <a href="https://www.linkedin.com/in/aryan-babare-49bb24298/" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
                                 </a>
-                                <a href="" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
+                                <a href="https://leetcode.com/u/aryanx16/" className=' border p-1 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-300 hover:text-neutral-700 dark:hover:bg-neutral-800 hover:bg-neutral-200 '>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-code"><polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/></svg>
                                 </a>
                             </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,40 @@ const Projects = () => {
 {/* project vines */}
         <div className='text-lg flex justify-between'>
           <div className='flex justify-center items-center gap-1'>
-            <a className='flex justify-center items-center gap-1' href="">
+            <a className='flex justify-center items-center gap-1' href="https://github.com/aryanx16/404CNF">
+
+            <div className='font-semibold'>LakeLens</div>
+             <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" /><path d="m21 3-9 9" /><path d="M15 3h6v6" /></svg>
+            </a>
+             |  <div className='bg-neutral-100  text-xs dark:text-neutral-100 dark:border-neutral-500 dark:bg-neutral-700 text-neutral-950 border border-neutral-300 px-2 py-0.5 rounded-md'>Flask</div>
+             <div className='bg-neutral-100  text-xs dark:text-neutral-100 dark:border-neutral-500 dark:bg-neutral-700 text-neutral-950 border border-neutral-300 px-2 py-0.5 rounded-md'>React</div>
+             <div className='bg-neutral-100  text-xs dark:text-neutral-100 dark:border-neutral-500 dark:bg-neutral-700 text-neutral-950 border border-neutral-300 px-2 py-0.5 rounded-md'>AWS S3</div>
+             </div>
+          <div>
+            <a href="https://github.com/aryanx16/coep-backend">
+
+            <div className=' border p-1 text-neutral-600 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-400 hover:text-neutral-800 dark:hover:bg-neutral-800 hover:bg-neutral-100 dark:text-neutral-500'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
+            </div>
+            </a>
+          </div>
+        </div>
+        <div>
+
+        </div>
+        <div className='text-sm dark:text-neutral-400 text-neutral-600 max-w-xl'>
+          <div>
+            • Developed a web-based metastore viewer to explore metadata from Parquet, Iceberg, Delta, and Hudi tables stored in object stores like AWS S3, Google Cloud Storage (GCS), Azure Blob Storage, and MinIO
+          </div>
+          • Integrated RESTful APIs to fetch structured metadata such as schema , partition keys, snapshot history, file metrics, row counts.
+          <div>
+            • Parsed complex metadata files like Iceberg manifests, Delta logs, and Hudi timeline files to extract versioned metadata and schema evolution.
+          </div>
+        </div>
+{/* project vines */}
+        <div className='mt-5 text-lg flex justify-between'>
+          <div className='flex justify-center items-center gap-1'>
+            <a className='flex justify-center items-center gap-1' href="https://vines.babare.site/">
 
             <div className='font-semibold'>Vines</div>
              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" /><path d="m21 3-9 9" /><path d="M15 3h6v6" /></svg>
@@ -19,7 +52,7 @@ const Projects = () => {
              <div className='bg-neutral-100  text-xs dark:text-neutral-100 dark:border-neutral-500 dark:bg-neutral-700 text-neutral-950 border border-neutral-300 px-2 py-0.5 rounded-md'>Cloudinary</div>
              </div>
           <div>
-            <a href="">
+            <a href="https://github.com/aryanx16/VINES">
 
             <div className=' border p-1 text-neutral-600 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-400 hover:text-neutral-800 dark:hover:bg-neutral-800 hover:bg-neutral-100 dark:text-neutral-500'>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
@@ -42,10 +75,10 @@ const Projects = () => {
             UI
           </div>
         </div>
-{/* project vines */}
+{/* Securecode vines */}
         <div className='text-lg flex justify-between mt-5'>
           <div className='flex justify-center items-center gap-1'>
-            <a className='flex justify-center items-center gap-1' href="">
+            <a className='flex justify-center items-center gap-1' href="https://securecode.babare.site/">
 
             <div className='font-semibold'>SecureCode</div>
              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" /><path d="m21 3-9 9" /><path d="M15 3h6v6" /></svg>
@@ -54,7 +87,7 @@ const Projects = () => {
              <div className='bg-neutral-100  text-xs dark:text-neutral-100 dark:border-neutral-500 dark:bg-neutral-700 text-neutral-950 border border-neutral-300 px-2 py-0.5 rounded-md'>Gemini API</div>
              </div>
           <div>
-            <a href="">
+            <a href="https://github.com/aryanx16/SecureCode">
 
             <div className=' border p-1 text-neutral-600 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-400 hover:text-neutral-800 dark:hover:bg-neutral-800 hover:bg-neutral-100 dark:text-neutral-500'>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
@@ -77,7 +110,7 @@ const Projects = () => {
 {/* project POSTWOMAN*/}
         <div className='text-lg flex justify-between mt-5'>
           <div className='flex justify-center items-center gap-1'>
-            <a className='flex justify-center items-center gap-1' href="">
+            <a className='flex justify-center items-center gap-1' href="https://github.com/aryanx16/PostWoman">
 
             <div className='font-semibold'>PostWoman</div>
              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" /><path d="m21 3-9 9" /><path d="M15 3h6v6" /></svg>
@@ -86,7 +119,7 @@ const Projects = () => {
              <div className='bg-neutral-100  text-xs dark:text-neutral-100 dark:border-neutral-500 dark:bg-neutral-700 text-neutral-950 border border-neutral-300 px-2 py-0.5 rounded-md'>REST APIs</div>
              </div>
           <div>
-            <a href="">
+            <a href="https://github.com/aryanx16/PostWoman">
 
             <div className=' border p-1 text-neutral-600 rounded-md dark:border-neutral-500 dark:hover:text-neutral-400 border-neutral-400 hover:text-neutral-800 dark:hover:bg-neutral-800 hover:bg-neutral-100 dark:text-neutral-500'>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
